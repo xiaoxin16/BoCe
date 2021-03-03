1c5753858378f8371b8fed05c88e886f0cdf45fc
XYH-PC <490482978@qq.com>
2021-03-03 23:12:56 +0800

    V1.0 OK

--- boce.py
+++ boce.py
@@ -39,7 +39,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 def main():
     # git config --global http.proxy 127.0.0.1:10809
-    # git config --global unset http.proxy
+    # git config --global --unset http.proxy
     print("start...")
 
 
